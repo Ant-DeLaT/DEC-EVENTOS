96b68af5f0b4f512dd9e36570aa3c6d8533b4e55
Ant-DeLaT <mailGoesHere@something.com>
2025-01-22 00:22:12 +0100

    Updated logics in js

--- js/superintendente.js
+++ js/superintendente.js
@@ -14,6 +14,25 @@ $("#vueltaLogin").click(
 $("#cierreSesion").click(
     localStorage.clear()
 )
+$().ready(
+$("#Identificarse").click(
+    // comparar pareja email-contraseña con entrantes
+    function comparaParejas(correVal,contraVal){
+        let val=false;
+        for (let i = 0; i < array.length; i++) {
+            let correoGuar = array[i,1];
+            let contraGuar = array[i,2];
+            // arrayCorreos.forEach(element => {
+            if (correVal==correoGuar && contraVal==contraGuar) {
+                location.replace("admin.html")
+                break;val=true;
+            }
+        }
+        (val)?null:alert("Correo o Contraseña no válidos")
+    }
+
+)
+)
 // Ojo ve
 // https://media.istockphoto.com/id/845329690/es/vector/icono-ojos-ilustraci%C3%B3n-vectorial.jpg?s=612x612&w=0&k=20&c=pfsZSCSwlqhdkgNrWqZYnmC98AHQsLTzDAQD8LO9SBs=
 // Ojo NO ve

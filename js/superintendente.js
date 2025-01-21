b86a1ffdbac2e5f081ea79382f5c6e0f37fce916
Ant-DeLaT <186017554+Ant-DeLaT@users.noreply.github.com>
2025-01-21 13:44:46 +0100

    Updated html and js

--- js/superintendente.js
+++ js/superintendente.js
@@ -1,3 +1,20 @@
+// alert("MORTADELO, FILEMÓN!!!!");
 // Vicente
-let arrayCorreos=Array(["Pepito Pérez","correo@prueba.com","1234contra"]);
+let arrayCorreos=Array(["Pepito Pérez","correo@prueba.com","1234contra"],["aa","test@test.com","contraseña"]);
 // 
+
+$("#vueltaRegistro").click(
+    $(".registro").css("display","block"),
+    $(".login").css("display","none")
+)
+$("#vueltaLogin").click(
+    $(".login").css("display","block"),
+    $(".registro").css("display","none"),
+)
+$("#cierreSesion").click(
+    localStorage.clear()
+)
+// Ojo ve
+// https://media.istockphoto.com/id/845329690/es/vector/icono-ojos-ilustraci%C3%B3n-vectorial.jpg?s=612x612&w=0&k=20&c=pfsZSCSwlqhdkgNrWqZYnmC98AHQsLTzDAQD8LO9SBs=
+// Ojo NO ve
+// https://cdn0.iconfinder.com/data/icons/interface-thin-line/32/ico-eye-closed-512.png
